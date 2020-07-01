68fa0a195f4abd29eda2a14a538f6148c25bea80
王海波 <wang.haibo@chinaott.net>
2020-07-01 17:56:44 +0800

    修改参数

--- communication/src/components/Child.vue
+++ communication/src/components/Child.vue
@@ -5,7 +5,7 @@
 	<button @click="getParent">执行父组件方法(this.$parent.handleParent())</button><br/>
 	<p v-if="parent">父组件方法的返回值 ： {{parent}}</p>
 	<button @click="getRoot">执行根组件方法(this.$root.handleRoot())</button>
-	<p v-if="parent">根组件方法的返回值 ： {{root}}</p>
+	<p v-if="root">根组件方法的返回值 ： {{root}}</p>
   </div>
 </template>
 
