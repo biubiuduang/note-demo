efa05e4a469b33097a754f55107027fb1a8a5231
王海波 <wang.haibo@chinaott.net>
2020-07-02 18:09:34 +0800

    插槽

---
+++ communication/src/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+	<p>Child</p>
+	<i>{{msg}}</i>
+	<p>第一个插槽(default)</p>
+	<slot></slot>
+	<p>第二个插槽(name="second")</p>
+	<slot name="second" :slotObj="slotObj"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+	data() {
+	  return {
+	    msg: '子组件的msg在子组件的作用域',
+	    slotObj: {
+	      firstName: '张三',
+		  lastName: '尼古拉斯'
+		}
+	  }
+	}
+  };
+</script>
+
+<style scoped>
+
+</style>
--- communication/src/views/Home.vue
+++ communication/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
     <p>Home</p>
+    <i>{{msg}}</i>
+    <Child>
+      <p>这是传给第一个插槽的内容</p>
+      <p slot="second">这是传给第二个插槽的内容</p>
+      <hr/>
+      <template v-slot:second="slotProps">
+        {{ slotProps.slotObj }}
+      </template>
+    </Child>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import Child from "../components/Child"
 export default {
   name: 'Home',
+  components: {
+    Child
+  },
+  data() {
+    return {
+      msg: '父组件的msg在父组件的作用域'
+    }
+  }
 }
 </script>
